93d05720597442b582282438b948ad6e6e1c3eb6
Ahmad Sazgar <a.sazgar@wallex.net>
2025-11-21 12:30:32 +0330

    req.method, req.url, req.response.status_code

---
+++ SeleniumWire-Headers.py
@@ -0,0 +1,31 @@
+from seleniumwire import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+def run_test():
+    chrome_options = Options()
+    chrome_options.add_argument("--ignore-certificate-errors")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+
+    driver = webdriver.Chrome(seleniumwire_options={
+        'verify_ssl': False
+    }, options=chrome_options)
+
+    driver.get("https://www.flytoday.ir/")
+    time.sleep(5)
+
+    print("=====NETWORK REQUESTS=====")
+    for req in driver.requests:
+        if req.response:
+            print("URL:", req.url)
+            print("Method:", req.method)
+            print("Status:", req.response.status_code)
+            print("Request Headers:", req.headers)
+            print("Response Headers:", req.response.headers)
+            print("=" * 50)
+
+    driver.quit()
+
+if __name__ == "__main__":
+    run_test()
